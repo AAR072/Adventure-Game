f4fa688f8593aa00b7a5c7df55e4db9bc451d7c4
AAR072 <aliakber@requity.ca>
2022-05-17 16:38:37 -0400

    Added while loop

--- Program.cs
+++ Program.cs
@@ -12,9 +12,15 @@ namespace adventuregame
             // Ask player name
             Console.WriteLine("Welcome to Escape the Tomb. A text based adventure game where your goal is to escape the tomb. What is your name?");
             player.askPlayerName();
-            Console.WriteLine($"Well {player.playerName}, you need to escape this room you are in. Look around by saying 'look'");
+            // Introduce player to looking
+            Console.WriteLine($"Well {player.playerName}, you need to escape this room you are in. Look around by saying 'look'.");
             playerInput = Console.ReadLine();
             player.getPlayerInput(playerInput);
+            while(player.gameState.Equals("alive")){
+                Console.WriteLine("What do you want to do?");
+                playerInput = Console.ReadLine();
+                player.getPlayerInput(playerInput);
+            }
             Console.WriteLine("Press any key to close...");
             Console.ReadKey();
         }
