4ec9024d1c24133503926d3be28ce6ee53099660
Ali-Akber <Ali-Akber@DESKTOP-MAC89A0>
2022-05-17 13:20:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace adventuregame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to close...");
+            Console.ReadKey();
+        }
+    }
+}
