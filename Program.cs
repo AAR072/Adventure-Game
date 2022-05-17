57afbc47987e45591b554c7274df51470bf22011
AAR072 <aliakber@requity.ca>
2022-05-17 14:10:07 -0400

    Added basic looking and name

--- Program.cs
+++ Program.cs
@@ -3,8 +3,18 @@ namespace adventuregame
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static string playerInput;
+
+        public static void Main()
         {
+            // Create new player
+            player player = new player();
+            // Ask player name
+            Console.WriteLine("Welcome to Escape the Tomb. A text based adventure game where your goal is to escape the tomb. What is your name?");
+            player.askPlayerName();
+            Console.WriteLine($"Well {player.playerName}, you need to escape this room you are in. Look around by saying 'look'");
+            playerInput = Console.ReadLine();
+            player.getPlayerInput(playerInput);
             Console.WriteLine("Press any key to close...");
             Console.ReadKey();
         }
