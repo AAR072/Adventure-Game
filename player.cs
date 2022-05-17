99e15be7a82f56e7058ddbcc871e4ee28d4adaf9
AAR072 <aliakber@requity.ca>
2022-05-17 16:38:18 -0400

    Updated looking

--- player.cs
+++ player.cs
@@ -9,10 +9,15 @@ namespace adventuregame
     internal class player
     {
         public static string playerName;
+        public static string gameState = "alive";
         // Array of all available actions
         public static string[] playerActions = {
-            //1
+            //0
             "look", 
+            };
+        public static string[] lookDirections = {
+            //0     1          2        3       4       5
+            "left", "right", "forward", "back", "up", "down"
 
         };
         // Function to ask the player's name
@@ -29,13 +34,34 @@ namespace adventuregame
         // Function to parse input and trigger required function
         public static void getPlayerInput(string textPlayerInput){
             textPlayerInput = textPlayerInput.ToLower();
-            for (int i = 0; i < playerActions.Length; i++){
-                if(textPlayerInput.Equals(playerActions[i])){
-                    Console.WriteLine($"The input is {playerActions[i]}");
+            for (int Iinput = 0; Iinput < playerActions.Length; Iinput++){
+                if(textPlayerInput.Equals(playerActions[Iinput])){
+                    Console.WriteLine(Iinput);
+                    if(Iinput == 0){
+                        look();
+                    }
 
                 }
             }
 
         }
+        // Function to check where to look
+        public static void look(){
+            Console.WriteLine("Which direction do you want to look in? You can look 'left', 'right', 'forward', 'back', 'up', 'down'");
+            string inputLookDirection = Console.ReadLine();
+            bool validLookDirection = false;
+            for (int Ilook = 0; Ilook < lookDirections.Length; Ilook++){
+                if(inputLookDirection.Equals(lookDirections[Ilook])){
+                    Console.WriteLine($"You look {lookDirections[Ilook]}");
+                    validLookDirection = true;
+                    break;
+                }
+                }
+            if(!validLookDirection){
+                Console.WriteLine("That is not a valid look direction");
+            }
+            
+            }
+        }
     }
-}
+
