2605d760e711024a1dcfa277ff086cc552e7c071
AAR072 <aliakber@requity.ca>
2022-05-18 13:16:57 -0400

    Added variables

--- room.cs
+++ room.cs
@@ -6,14 +6,30 @@ using System.Threading.Tasks;
 
 namespace adventuregame
 {
-    
+
     internal class room
     {
-        public static string[] roomTypes = {
+
+        public string leftRoomObject = "";
+        public string rightRoomObject = "";
+        public string upRoomObject = "";
+        public string downRoomObject = "";
+        public string forwardRoomObject = "";
+        public string backRoomObject = "";
+        public string roomType { get; set; }
+        public string[] roomTypes = {
             //0           1          2           3       4       5
             "entrance", "corridor", "library", "jail", "pit", "ending"
 
         };
+
+        public room(string tempRoomType)
+        {
+            roomType = tempRoomType;
+
+        }
+
+
 
     }
 }
