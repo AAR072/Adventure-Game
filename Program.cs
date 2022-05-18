67a7549e8ff0002a165f99bb553c6080faf8f126
AAR072 <aliakber@requity.ca>
2022-05-18 13:16:41 -0400

    Added inventory tutorial

--- Program.cs
+++ Program.cs
@@ -9,25 +9,55 @@ namespace adventuregame
         {
             // Create new player
             player player = new player();
-            room room = new room();
+            room entrance = new room("entrance");
+            // Setting up entrance room
+            entrance.leftRoomObject = player.availableObjects[0];
+            entrance.rightRoomObject = player.availableObjects[5];
+            room currentRoom = entrance;
+
             // Ask player name
-            Console.WriteLine("Welcome to Escape the Tomb. A text based adventure game where your goal is to escape the tomb. What is your name?");
+            Console.WriteLine("Welcome to Escape the Tomb. A text based adventure game where your goal is to escape. What is your name?");
             player.askPlayerName();
+
             // Introduce player to looking
-            Console.WriteLine($"Well {player.playerName}, you need to escape this room you are in. Look around by saying 'look'.");
+            Console.WriteLine($"Well {player.playerName}, you need to escape this room you are in. Look around by saying 'look'. You will be prompted to enter a direction. Explore the room around you.");
+
+            // Introduce player to giving input
             playerInput = Console.ReadLine();
-            player.getPlayerInput(playerInput);
-
-
-            // Loop while alive
-            while(player.gameState.Equals("alive")){
-                Console.WriteLine("What do you want to do?");
-                playerInput = Console.ReadLine();
-                player.getPlayerInput(playerInput);
+            player.playerInputResult = player.getPlayerInput(playerInput);
+            // If player wants to look
+            if (player.playerInputResult.Equals("look"))
+            {
+                player.look(currentRoom);
+            }
+            // Introduce player to inventory
+            Console.WriteLine($"Items you pick up are stored in your inventory. View it by writing 'inv'");
+            playerInput = Console.ReadLine();
+            player.playerInputResult = player.getPlayerInput(playerInput);
+            if (player.playerInputResult.Equals("look"))
+            {
+                player.look(currentRoom);
+            }
+            if (player.playerInputResult.Equals("inv")){
+                Console.WriteLine($"Inventory");
             }
 
+            // Loop while alive
+            while (player.gameState.Equals("alive"))
+            {
+                Console.WriteLine("What do you want to do?");
+                playerInput = Console.ReadLine();
+                player.playerInputResult = player.getPlayerInput(playerInput);
+                // If player wants to look
+                if (player.playerInputResult.Equals("look"))
+                {
+                    // Look left using current room 
+                    player.look(entrance);
+                }
+            }
             Console.WriteLine("Press any key to close...");
             Console.ReadKey();
         }
     }
 }
+// AA wuz here help me
