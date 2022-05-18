7219a9ed1339873ffd27c71f3763863970c73f17
AAR072 <aliakber@requity.ca>
2022-05-17 23:12:24 -0400

    Created room class

---
+++ room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventuregame
+{
+    
+    internal class room
+    {
+        public static string[] roomTypes = {
+            //0           1          2           3       4       5
+            "entrance", "corridor", "library", "jail", "pit", "ending"
+
+        };
+
+    }
+}
