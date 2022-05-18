79826ae3851a5805d6d08153eff7cd50a976695b
AAR072 <aliakber@requity.ca>
2022-05-17 23:12:09 -0400

    Added support for room class

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace adventuregame
         {
             // Create new player
             player player = new player();
+            room room = new room();
             // Ask player name
             Console.WriteLine("Welcome to Escape the Tomb. A text based adventure game where your goal is to escape the tomb. What is your name?");
             player.askPlayerName();
@@ -16,11 +17,15 @@ namespace adventuregame
             Console.WriteLine($"Well {player.playerName}, you need to escape this room you are in. Look around by saying 'look'.");
             playerInput = Console.ReadLine();
             player.getPlayerInput(playerInput);
+
+
+            // Loop while alive
             while(player.gameState.Equals("alive")){
                 Console.WriteLine("What do you want to do?");
                 playerInput = Console.ReadLine();
                 player.getPlayerInput(playerInput);
             }
+
             Console.WriteLine("Press any key to close...");
             Console.ReadKey();
         }
